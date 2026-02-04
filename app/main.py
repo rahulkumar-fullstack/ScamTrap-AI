ce93ef69991643bfed758e8e4eb4823f49f6ca31
Rahulkumar Gupta <159034627+rahulkumar-fullstack@users.noreply.github.com>
2026-02-05 05:03:47 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI, Depends
 from app.auth import verify_api_key
+from app.scam_detector import is_scam
 
 app = FastAPI(title="ScamTrap AI Honeypot")
 
+@app.post("/detect", dependencies=[Depends(verify_api_key)])
+def detect(message: dict):
+    text = message.get("text", "")
+    result = is_scam(text)
+
+    return {
+        "scamDetected": result
+    }
+
+'''
 @app.get("/", dependencies=[Depends(verify_api_key)])
 def home():
     return {"message": "ScamTrap AI is running 🚀"}
@@ -10,3 +21,4 @@ def home():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+'''
