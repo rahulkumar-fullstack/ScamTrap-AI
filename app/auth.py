512463b5656e7e80de19a686c06a6ac08be0591e
Rahulkumar Gupta <159034627+rahulkumar-fullstack@users.noreply.github.com>
2026-02-05 04:38:40 +0530

    Create auth.py
    Add Auth to verify API KEy

---
+++ app/auth.py
@@ -0,0 +1,6 @@
+from fastapi import Header, HTTPException
+from app.config import settings
+
+def verify_api_key(x_api_key: str = Header(...)):
+    if x_api_key != settings.API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API key")
