c14a5000bb1f08f26a4710388889d13f1ec910d4
Rahulkumar Gupta <159034627+rahulkumar-fullstack@users.noreply.github.com>
2026-02-06 01:59:39 +0530

    add verify api key

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,7 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Header, HTTPException
+from app.core.config import settings
 
 router = APIRouter()
 
+def verify_api_key(x_api_key: str = Header(...)):
+    if x_api_key != settings.api_key:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
 @router.get("/")
-async def root():
+async def root(x_api_key: str = Header(...)):
+    verify_api_key(x_api_key)
     return {"message": "ScamTrap AI Async API is running!"}
