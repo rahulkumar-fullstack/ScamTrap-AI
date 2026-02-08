1a1a8ec5dfd0ae32a8399ab2f3de2fd1c30f858f
Rahulkumar Gupta <159034627+rahulkumar-fullstack@users.noreply.github.com>
2026-02-09 01:19:06 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,6 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     api_key: str
     minilm_model: str
-    callback_url: str
 
     class Config:
         env_file = ".env"
