15b27ec84e69ae4aeb3cfc24ece637ae12afdb66
Rahulkumar Gupta <159034627+rahulkumar-fullstack@users.noreply.github.com>
2026-02-09 01:50:44 +0530

    Update model.py

--- app/models/model.py
+++ app/models/model.py
@@ -5,19 +5,15 @@ from app.core.config import settings
 _model = None
 _lock = asyncio.Lock()
 
-async def load_model():
+async def get_model():
     global _model
-    async with _lock:  # prevent race condition
-        if _model is None:
-            loop = asyncio.get_event_loop()
-
-            _model = await loop.run_in_executor(
-                None,
-                lambda: SentenceTransformer(settings.minilm_model)
-            )
-    return _model
-
-def get_model():
     if _model is None:
-        raise RuntimeError("Model not loaded. Call load_model() at startup.")
+        async with _lock:
+            if _model is None:  # double-checked locking
+                # Load model off the main thread
+                loop = asyncio.get_event_loop()
+                _model = await loop.run_in_executor(
+                    None,
+                    lambda: SentenceTransformer(settings.minilm_model)
+                )
     return _model
