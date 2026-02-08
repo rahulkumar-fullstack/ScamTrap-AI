71d94619f9c7a29b73731cde65dc00606d3d6bff
Rahulkumar Gupta <159034627+rahulkumar-fullstack@users.noreply.github.com>
2026-02-08 22:30:06 +0530

    Add lifespan context manager to load model during FastAPI startup

--- app/main.py
+++ app/main.py
@@ -1,18 +1,32 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
 from app.api.routes import router
+from app.models.model import load_model
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #print("Loading MiniLM model...")
+    await load_model()
+    #print("Model loaded successfully.")
+    yield
+   #print("Shutting down application...")
+
 
 # Create FastAPI instance
 app = FastAPI(
     title="ScamTrap AI Honeypot",
-   description="""
+    description="""
 Async AI-powered honeypot for scam detection.
 
 **Developer:** [Rahul Kumar](https://github.com/rahulkumar-fullstack)
 
 **License:** [MIT](https://opensource.org/licenses/MIT)
 """,
-    version="2.0"
+    version="2.0",
+    lifespan=lifespan
 )
 
-# Include routes from router
+# Include routes
 app.include_router(router)
