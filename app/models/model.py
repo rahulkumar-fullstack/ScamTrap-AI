87a15369047d3fbd0220fc1ecbf06439847a6a91
Rahulkumar Gupta <159034627+rahulkumar-fullstack@users.noreply.github.com>
2026-02-10 15:12:40 +0530

    Update model.py

--- app/models/model.py
+++ app/models/model.py
@@ -1,6 +1,5 @@
 import asyncio
 from sentence_transformers import SentenceTransformer
-from app.core.config import settings
 
 _model = None
 _lock = asyncio.Lock()
@@ -14,6 +13,6 @@ async def get_model():
                 loop = asyncio.get_event_loop()
                 _model = await loop.run_in_executor(
                     None,
-                    lambda: SentenceTransformer(settings.minilm_model)
+                    lambda: SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
                 )
     return _model
